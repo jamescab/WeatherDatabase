038099afb00cc2de1b8212d5fe4d30cd6e4d5de9
James Cabaral <jamescabaral@gmail.com>
2023-01-30 16:16:35 -0800

    Removed info

--- root/advisories.php
+++ root/advisories.php
@@ -139,7 +139,8 @@
 
             // Your username is ora_(CWL_ID) and the password is a(student number). For example,
 			// ora_platypus is the username and a12345678 is the password.
-            $db_conn = OCILogon("ora_jamesc17", "a31046386", "dbhost.students.cs.ubc.ca:1522/stu");
+            //Login info removed for security reasons. Enter login info here before running app.
+	    //$db_conn = OCILogon();
 
             if ($db_conn) {
                 debugAlertMessage("Database is Connected");
